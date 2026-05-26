955dcbe20210298fd4766a903b8e39979dc410d6
unknown <growithpro.in@gmail.com>
2026-05-26 11:09:19 +0530

    Fixed resend commit

--- api/submit.js
+++ api/submit.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
   try {
 
     await resend.emails.send({
-      from: '<anything>@saaelki.resend.app',
+      from: 'MCQ Test <anything@saaelki.resend.app>',
       to: 'naveen.joshi.net@gmail.com',
       subject: 'New MCQ Test Result',
       html: `
