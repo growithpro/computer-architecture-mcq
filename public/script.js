a50bbc771a41297838cb5a2a4961f66c925bf44d
unknown <growithpro.in@gmail.com>
2026-06-02 13:29:55 +0530

    sheet  commit3

--- public/script.js
+++ public/script.js
@@ -223,10 +223,13 @@ async function submitQuiz() {
             "https://script.google.com/macros/s/AKfycbzxwpmRPPpS6ErkMxmkCO-u7AwHazmAQUohpPNrAgalZ_corilCW2NLrTdAkK9WV2hSLw/exec",
             {
                 method: "POST",
+                headers: {
+                    "Content-Type": "text/plain;charset=utf-8"
+                },
                 body: JSON.stringify({
-                    name,
-                    email,
-                    score
+                    name: name,
+                    email: email,
+                    score: score
                 })
             }
         );
